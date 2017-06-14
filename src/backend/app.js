66fd467c94fd6c39d1eaf44a935d298e1c16e671
zainamir4 <zain_1071@hotmail.com>
2017-06-13 23:51:35 -0400

    added mongodb

--- src/backend/app.js
+++ src/backend/app.js
@@ -15,6 +15,18 @@ app.use(function (req, res, next){
   console.log("HTTPS Response", res.statusCode);
 });
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var url = 'mongodb://localhost:27017/horizon';
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
+
 
 https.createServer(app).listen(3000, function () {
   console.log('HTTPS on port 3000');
