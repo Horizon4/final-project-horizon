b26c4d9094b599105263e86ae278b736692cefc8
HouleNemo <hao.howard.chen@gmail.com>
2017-06-18 16:43:47 -0400

    [#147244213] added api calls to backend

--- src/frontend/landing/scripts/view.js
+++ src/frontend/landing/scripts/view.js
@@ -8,7 +8,7 @@ var view = (function() {
 
     return {
         init: function() {
-
+            sign_in_up.init();
         },
     }
 })();
@@ -16,9 +16,34 @@ var view = (function() {
 
 var sign_in_up = (function() {
 
+    function submitSignIn(e) {
+        e.preventDefault();
+
+        var username = $("#sign_in_user").val();
+        var password = $("#sign_in_pass").val();
+
+        controller.signin(username, password);
+    }
+
+    function submitSignUp(e) {
+        e.preventDefault();
+
+        var username = $("#sign_up_user").val();
+        var password = $("#sign_up_pass").val();
+        var confirmation = $("#sign_up_conf").val();
+
+        if (password === confirmation) {
+            controller.signup(username, password);
+        } else {
+            // TODO create error message on webpage
+            console.error("password does not match");
+        }
+    }
+
     return {
         init: function() {
-            
+            $("#sign_in_btn").click(submitSignIn);
+            $("#sign_up_btn").click(submitSignUp);
         }
     }
 })();
