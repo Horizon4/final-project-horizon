77ae92afb52b0188b1326add1ae0bb09ffd3df89
HouleNemo <hao.howard.chen@gmail.com>
2017-06-17 22:28:20 -0400

    initial front end setup with jquery

---
+++ src/frontend/__Template/readme.md
@@ -0,0 +1,4 @@
+Note: This follows a MVC setup.
+Model - communicates with the backend
+Controller - handles data manipulation (if any)
+View - displays content dynamically
---
+++ src/frontend/__Template/scripts/view.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    controller.init();
+    view.init();
+}
+
+var view = (function() {
+
+    return {
+        init: function() {
+
+        },
+    }
+});
