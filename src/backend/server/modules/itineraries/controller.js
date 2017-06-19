f6a64923ebca6b86d45b4d16aadddabd6c2c0573
csbrown9338 <csbrown9338@gmail.com>
2017-06-19 02:58:04 -0400

    [#147244699] automatically adds a flight object into the flights list for a newly created itinerary.

--- src/backend/server/modules/itineraries/controller.js
+++ src/backend/server/modules/itineraries/controller.js
@@ -1,7 +1,18 @@
 import Itinerary from './model';
 
 export const createItinerary = (req, res) => {
-  const newItinerary = new Itinerary()
+  /*const flight1 = {'id':1}
+  const flight2 = {'id':2}
+  const arrayFlights = [flight1, flight2]
+  const newItinerary = new Itinerary({ flights: arrayFlights}); */
+  
+  const ({ origin, destination, departureDate, returnDate, adultCount, childCount}) = req.body
+  const newItinerary = new Itinerary({ origin, destination, departureDate, returnDate, adultCount, childCount });
+
+  const newFlight = addFlight({ newItinerary.origin, newItinerary.destination, newItinerary.departureDate, newItinerary.returnDate,
+    4, newItinerary.adultCount, "Cabin Type", "Carrier Type", newItinerary.childCount });
+
+  newItinerary.flights.push(newFlight);
 
   newItinerary.save()
   .then((itinerary) => {
@@ -10,4 +21,12 @@ export const createItinerary = (req, res) => {
   .catch((err) => {
     return res.status(500).json({ error: true, message: 'Trouble adding the itinerary'});
   })
+}
+
+export const addFlight = (req, res) => {
+  const ({ origin, destination, departureDate, returnDate, price, adultCount, cabin, carrier, childCount}) = req.body
+  const newFlight = new Flight({ origin, destination, departureDate, returnDate, price, adultCount, cabin, carrier, childCount });
+
+  return newFlight; // how do you actually return this?
+
 }
--- src/backend/server/modules/itineraries/model.js
+++ src/backend/server/modules/itineraries/model.js
@@ -1,15 +1,38 @@
 import mongoose, { Schema } from 'mongoose';
 
 const itinerarySchema = new Schema({
+  origin: {
+    type: String,
+    required: true
+  },
+  destination: {
+    type: String,
+    required: true
+  },
+  departureDate: {
+    type: Date,
+    required: true
+  },
+  returnDate: {
+    type: Date,
+    required: true
+  },
+  adultCount: {
+    type: Number
+  },
+  childCount: {
+    type: Number
+  },
   flights: {
     type:[{}],
     required: false
-  }
-  /* stopOvers: {
+  },
+  stopOvers: {
     type: [String],
-  } */
+  }
   stopoverCount: {
     type: Number,
+    default: 0
   }
 });
 
