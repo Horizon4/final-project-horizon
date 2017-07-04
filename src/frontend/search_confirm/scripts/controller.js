ac8d238db4ae3529a992eaa7cdd43c3544c4c7a3
HouleNemo <hao.howard.chen@gmail.com>
2017-07-03 21:59:30 -0400

    [#147729277] finished repacking data returned from api call

--- src/frontend/search_confirm/scripts/controller.js
+++ src/frontend/search_confirm/scripts/controller.js
@@ -5,21 +5,15 @@ var controller = (function() {
             model.init();
         },
 
-//        send: function(origin, destination, price, departureDate, returnDate) {
-//            var data = {
-//                'origin':origin,
-//                'destination': destination,
-//                'price': price,
-//                'departureDate': departureDate,
-//                'returnDate': returnDate
-//            };
-            //console.log(data); //DEBUG
-//           var result = model.send(data);
-//            if (result.status == 200) {
-//                return true;
-//            } else {
-//                return false;
-//            }
-//       },
+        getNumItineries: function() {
+            return model.getNumItineries();
+        },
+        getFlights: function(idx) {
+            return model.getFlights(idx);
+        },
+        getAccommodations: function(idx) {
+            return model.getAccommodations(idx);
+        },
+
     }
 })();
--- src/frontend/search_confirm/scripts/view.js
+++ src/frontend/search_confirm/scripts/view.js
@@ -7,30 +7,7 @@ var view = (function() {
 
     return {
         init: function() {
-            confirm.init();
+            
         },
-    }
-})();
-
-
-var confirm = (function() {
-
-    function searchSubmit(e) {
-        e.preventDefault();
-
-        $(".msg").hide().removeClass("error success");
-
-
-
-        /*if (!success){
-            msg.text("Error sending fields").addClass("error");
-            msg.show();
-        }*/
-    }
-
-    return {
-        init: function() {
-            $("#submitButton").click(searchSubmit);
-        }
     }
 })();
