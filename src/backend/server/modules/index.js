33204ce59b76dd3ae16e700b42fc1b44827eb8e2
zainamir4 <zain_1071@hotmail.com>
2017-07-12 16:50:08 -0400

    setup the attractions folder

--- src/backend/server/modules/index.js
+++ src/backend/server/modules/index.js
@@ -3,3 +3,4 @@ export * from './flight/index';
 export * from './itineraryProcess/index';
 export * from './itinerary/index';
 export * from './accommodation/index';
+export * from './attractions/index';
