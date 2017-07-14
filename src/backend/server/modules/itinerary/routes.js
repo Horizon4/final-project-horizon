e0bd6f1f3b42843977c6d33466550e394d99d631
shumjas1 <jas.shum@mail.utoronto.ca>
2017-07-14 00:45:44 -0400

    #147245507 added functionality to support recommendations and ratings for itineraries in the backend, updated README api docs as well

--- src/backend/server/modules/itinerary/routes.js
+++ src/backend/server/modules/itinerary/routes.js
@@ -6,5 +6,7 @@ const routes = new Router();
 routes.post('/createItinerary', ItineraryController.createItinerary);
 routes.put('/selected', ItineraryController.selected);
 routes.get('/completed/:username', ItineraryController.completedItinerary);
+routes.put('/updateRating', ItineraryController.updateRating);
+routes.put('/updateRecommendations', ItineraryController.updateRecommendations);
 
 export default routes;
